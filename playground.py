c65ca60fc9a3a91f19df0c656bcdf52faf6c75c2
Larry Kieu <86716937+dqieu@users.noreply.github.com>
2026-02-04 22:55:41 -0500

    initial msg

--- playground.py
+++ playground.py
@@ -1,51 +1,63 @@
+import argparse
+
 import torch
 import torch.nn as nn
 from tiny_recursive_model import TinyRecursiveModel, MLPMixer1D, Trainer
 from tiny_recursive_model.transformer import init_transformer
 
-transformer_layer = nn.TransformerEncoderLayer(
-    d_model = 768,
-    nhead = 8,
-    dim_feedforward=1536,
-    batch_first=True,
-    norm_first=True,
-)
+if __name__ == '__main__':
+    # Parsing arguments
+    parser = argparse.ArgumentParser(description="Train Tiny Recursive Model")
+    parser.add_argument('--data-path', '-p', type=str, required=True, help='Path to the HDF5 dataset')
+    parser.add_argument('--excluded-idx', '-e', type=int, nargs='*', default=[], help='Indices to exclude from the dataset')
+    parser.add_argument('--batch-size', '-b', type=int, default=16, help='Batch size for training')
+    parser.add_argument('--epochs', type=int, default=10, help='Number of training epochs')
+    parser.add_argument('--output-path', '-o', type=str, required=True, help='Path to save the trained model')
+    args = parser.parse_args()
 
-trm = TinyRecursiveModel(
-    dim = 768,
-    # network = nn.TransformerEncoder(
-    #     transformer_layer,
-    #     num_layers = 2
-    # ),
-    network = MLPMixer1D(
-        dim = 768,
-        depth = 1,
-        seq_len = 256,
-    ),
-    use_cls_token=True,
-    num_refinement_blocks=2,
-    num_latent_refinements=3
-)
-# trm.network.apply(init_transformer)
+    # transformer_layer = nn.TransformerEncoderLayer(
+    #     d_model = 768,
+    #     nhead = 8,
+    #     dim_feedforward=1536,
+    #     batch_first=True,
+    #     norm_first=True,
+    # )
 
-#%%
-from torch.utils.data import Subset
-from tiny_recursive_model.dataio import HDF
+    trm = TinyRecursiveModel(
+        dim=768,
+        # network = nn.TransformerEncoder(
+        #     transformer_layer,
+        #     num_layers = 2
+        # ),
+        network=MLPMixer1D(
+            dim=768,
+            depth=1,
+            seq_len=256,
+        ),
+        use_cls_token=True,
+        num_refinement_blocks=2,
+        num_latent_refinements=3
+    )
+    # trm.network.apply(init_transformer)
 
-hdf_ds = HDF("/Users/lkieu/PycharmProjects/Audio-Health-Benchmark/tmp/torgo/wavlm-basep-last/cache.hdf5",
-         excluded_idx=[1685])
+    from torch.utils.data import Subset
+    from tiny_recursive_model.dataio import HDF
 
-train_ds = Subset(hdf_ds, hdf_ds.split_idx[0])
-val_ds   = Subset(hdf_ds, hdf_ds.split_idx[1])
-test_ds  = Subset(hdf_ds, hdf_ds.split_idx[2])
+    # "/Users/lkieu/PycharmProjects/Audio-Health-Benchmark/tmp/torgo/wavlm-basep-last/cache.hdf5"
+    hdf_ds = HDF(args.data_path,
+                 excluded_idx=args.excluded_idx,)
 
+    train_ds = Subset(hdf_ds, hdf_ds.split_idx[0])
+    val_ds = Subset(hdf_ds, hdf_ds.split_idx[1])
+    test_ds = Subset(hdf_ds, hdf_ds.split_idx[2])
 
-#%%
-trainer = Trainer(
-    trm,
-    train_ds,
-    epochs = 1,
-    batch_size = 16,
-    cpu = False
-)
-trainer()
+    trainer = Trainer(
+        trm,
+        train_ds,
+        epochs=args.epochs,
+        batch_size=args.batch_size,
+        cpu=False
+    )
+    trainer()
+
+    torch.save(trm.state_dict(), args.output_path)
